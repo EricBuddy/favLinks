462c12a76fe30a0f126b6aaf4fb00ae9253c5408
EricBuddy <Eric.Buddy00@gmail.com>
2021-10-08 20:19:45 -0400

    added the list to appear on web

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,13 +8,33 @@ class Form extends Component {
             TODO - set initial state for link name and URL 
 
         */
+       this.state={
+           name:'',
+           URL:''
+       }
     }
 
-    handleChange = event => {
-        /*
-            TODO - Logic for changing state based on form changes
-        */
+    // handleChange = event => {
+    //     /*
+    //         TODO - Logic for changing state based on form changes
+    //     */
+    //    this.setState({
+    //        name: event.target.value,
+    //        URL: event.target.value
+    //    })
+    // }
+    handleChangeName = event=>{
+        this.setState({
+            name:event.target.value
+        })
     }
+
+    handleChangeURL = event=>{
+        this.setState({
+            URL:event.target.value
+        })
+    }
+
 
     onFormSubmit = (event) => {
         // to prevent page reload on form submit
@@ -23,14 +43,34 @@ class Form extends Component {
         /*
             TODO - Logic for calling props to handle submission and setting state changes
         */
-
+        this.props.handleSubmit(this.state)
+        //this will empty the text box after the submit button is click
+        this.setState({
+            name: '',
+            URL: ''
+        })
     }
 
     render() {
 
         return(
             <form>
-                {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+                
+            {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+            <label>Name</label>
+          {/* // {this.state.name}  */}
+            <input 
+            type='text' 
+            value={this.state.name} 
+            onChange={this.handleChangeName}
+            required/>
+            <label>URL</label>
+            <input 
+            type='text' 
+            value={this.state.URL} 
+            onChange={this.handleChangeURL}
+            required/>
+            <button onClick={this.onFormSubmit}>Submit</button>
             </form>
         )
     
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,23 +1,38 @@
 import React from 'react'
-// import Table from './Table';
-// import Form from './Form';
+import Table from './Table';
+import Form from './Form';
 
 class LinkContainer extends React.Component {
   constructor(props) {
     super(props)
     /* TODO - Create state object for storing favLinks */
+    this.state={
+      favLinks:[]
+    }
   }
 
   handleRemove = (index) => {
     /*
             TODO - Create logic for setting the state to filter array and remove favLink at index
         */
+      //  let fav = [...this.state.favLinks]
+      //  let favl = fav.indexOf(index.target.value)
+      //  if (favl !== -1) {
+      //    fav.splice(favl, 1);
+      //    this.setState({favLinks: fav});
+      //  }
+    
+       
   }
+  
 
   handleSubmit = (favLink) => {
     /*
             TODO - Create logic to setState and add new favLink to favLinks array in state
         */
+       let oldLinks=this.state.favLinks
+       this.setState({favLinks:[...oldLinks,favLink]})
+       
   }
 
   render() {
@@ -26,11 +41,13 @@ class LinkContainer extends React.Component {
         <h1>My Favorite Links</h1>
         <p>Add a new url with a name and link to the table.</p>
         {/*TODO - Add Table Component */}
-
+        <Table linkData={this.state.favLinks}
+        removeLink={this.handleRemove}/>
         <br />
 
         <h3>Add New</h3>
         {/*TODO - Add Form Component */}
+        <Form handleSubmit={this.handleSubmit}/>
       </div>
     )
   }
